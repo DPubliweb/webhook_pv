cfc688bee5a40e3a8810f0ccc66a74a551336d95
Johann2626 <johann@publiweb.agency>
2025-03-21 11:30:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -207,7 +207,7 @@ clientInterests = {
             #'Samy Naccache LR': [87,19,23,31,81,82],
             #'Samy Nackache AS': [87,19,86,36,23,45,10,89],
             'Samy Nackache THC': [22,29,56,35],
-            'Samy Naccache GENCY ' : [21,1,71,39],
+            'Samy Naccache LR ' : [31,81,82,32,9],
             #'Samy Nackache NAKISS': [54,55,57,70,88],
             #'Alex Benhamou' : [76, 27, 28, 45],
         }
